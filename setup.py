57be7286d86c1387c6e950c04f003951db58acf3
Jon Chen <bsd@voltaire.sh>
2014-07-31 22:54:08 -0400

    init

---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import, division, print_function
+
+import os
+import setuptools
+
+base_dir = os.path.dirname(__file__)
+
+about = {}
+with open(os.path.join(base_dir, "yaurtww", "__about__.py")) as f:
+    exec(f.read(), about)
+
+with open(os.path.join(base_dir, "README.rst")) as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name=about["__title__"],
+    version=about["__version__"],
+
+    description=about["__summary__"],
+    long_description=long_description,
+    license=about["__license__"],
+    url=about["__uri__"],
+
+    author=about["__author__"],
+    author_email=about["__email__"],
+
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Games/Entertainment :: First Person Shooters"
+    ],
+
+    packages=["yaurtww"],
+    install_requires=[
+        "grequests==0.2.0",
+    ],
+    entry_points={
+        "console_scripts": ["yaurtww=yaurtww:main"]
+    }
+)
---
+++ yaurtww/__about__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, division, print_function
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "yaurtww"
+__summary__ = "Yet Another Urban Terror Wget Wrapper."
+__uri__ = "https://github.com/urtdevs/yaurtww"
+
+__version__ = "0.0.1"
+
+__author__ = "Jon Chen"
+__email__ = "bsd@voltaire.sh"
+
+__license__ = "BSD 3-Clause"
+__copyright__ = "Copyright 2014 %s" % __author__
